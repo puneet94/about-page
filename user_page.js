3dbd1cd11ce42babc57703ab6a050ef5613957a4
Puneet Mahendra <puneet.mahendra94@gmail.com>
2015-10-05 13:52:13 +0530

    header intergrated

--- user_page.js
+++ user_page.js
@@ -1,5 +1,51 @@
 (function(){
     $(document).ready(function(){
+      
+        $('.toggle_button').on('click',function(){
+            $('.sideMenu').height($(document).height());
+          $('.sideMenu').animate({'right':'0%'},function(){
+            $('.sideMenuHideArea').css('background','rgba(0,0,0,0.7)');   
+          });
+          $('.sideMenu').data('shown','1');
+        });
+        $('.sideMenuHideArea').on('click',function(){
+          $('.sideMenuHideArea').css('background','transparent'); 
+          $('.sideMenu').animate({'right':'100%'});
+          $('.sideMenu').data('shown','0');
+        });
+        $(".searchSectionIconButton").click(function(){
+        $(".fullSearch").show();
+            $(window).scrollTop(0);
+            console.log($(".fullSearch").scrollTop());
+            $(".fullSearch").height($(document).height());
+    });
+    $('body').on('click','.searchSectionCloseButton',function(){
+      $(".fullSearch").hide();
+    });
+        $(".categoriesLink").click(function(){
+            $(".inputPopUp").show();
+            $(window).scrollTop(0);
+            console.log($(".inputPopUp").scrollTop());
+        });
+        $(".locationsLink").click(function(){
+            $(".locationPopUp").show();
+            $(window).scrollTop(0);
+            console.log($(".locationPopUp").scrollTop());
+        });
+        $(".inputPopUpClose").click(function(){
+            $(".inputPopUp").hide();
+        });
+        $(".locationPopUpClose").click(function(){
+            $(".locationPopUp").hide();
+        });
+        $(".searchSectionLocationInput, .searchSectionStoreInput").blur(function(){
+            $(window).scrollTop(0);
+        });
+        $(".searchSectionLocationInput, .searchSectionStoreInput").keyup(function(event){
+            if(event.keyCode == 13){
+                $(window).scrollTop(0);
+            }
+        });
     	wow = new WOW(
                       {
                       boxClass:     'wow',      // default
