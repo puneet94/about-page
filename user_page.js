a809503c0c44727c4f748cefb3a553dac0297440
Puneet Mahendra <puneet.mahendra94@gmail.com>
2015-10-13 12:42:04 +0530

    cross

--- user_page.js
+++ user_page.js
@@ -1,39 +1,49 @@
 (function(){
     $(document).ready(function(){
+        var filesJSON = [];
+        
+
+
         $('.videoPopUp').hide();
+        $('#videoPlayButton').on('click',function(){
+            $('#vidtest').click();
+            $('.videoPopUp').fadeIn( "slow");
             $('#vidtest').height($(window).height());
-            $('#videoPlayButton').on('click',function(){
-                $('#vidtest').click();
-                $('#vidtest').get(0).play();
-                $('.videoPopUp').fadeIn( "slow");
-                
-               
-            });
-            $('#closeVideoButton').on('click',function(){
-                $('.videoPopUp').fadeOut();
-               $('#vidtest').get(0).pause();
-            });
+            var vid = $('#vidtest').get(0);
+            vid.play();
+            /*if (vid.requestFullscreen) {
+                vid.requestFullscreen();
+            } else if (vid.mozRequestFullScreen) {
+                vid.mozRequestFullScreen();
+            } else if (vid.webkitRequestFullscreen) {
+                vid.webkitRequestFullscreen();
+            }*/
+        });
+        $('#closeVideoButton').on('click',function(){
+            $('.videoPopUp').fadeOut();
+            $('#vidtest').get(0).pause();
+        });
         $('.toggle_button').on('click',function(){
             $('.sideMenu').height($(document).height());
-          $('.sideMenu').animate({'right':'0%'},function(){
-            $('.sideMenuHideArea').css('background','rgba(0,0,0,0.7)');   
-          });
-          $('.sideMenu').data('shown','1');
+            $('.sideMenu').animate({'right':'0%'},function(){
+                $('.sideMenuHideArea').css('background','rgba(0,0,0,0.7)');   
+            });
+            $('.sideMenu').data('shown','1');
         });
         $('.sideMenuHideArea').on('click',function(){
           $('.sideMenuHideArea').css('background','transparent'); 
           $('.sideMenu').animate({'right':'100%'});
           $('.sideMenu').data('shown','0');
-        });
+      });
         $(".searchSectionIconButton").click(function(){
-        $(".fullSearch").show();
+            $(".fullSearch").show();
             $(window).scrollTop(0);
             
             $(".fullSearch").height($(document).height());
-    });
-    $('body').on('click','.searchSectionCloseButton',function(){
-      $(".fullSearch").hide();
-    });
+        });
+        $('body').on('click','.searchSectionCloseButton',function(){
+          $(".fullSearch").hide();
+      });
         $(".categoriesLink").click(function(){
             $(".inputPopUp").show();
             $(window).scrollTop(0);
@@ -58,33 +68,33 @@
                 $(window).scrollTop(0);
             }
         });
-    	wow = new WOW(
-                      {
+        wow = new WOW(
+        {
                       boxClass:     'wow',      // default
                       animateClass: 'animated', // default
                       offset:       100,          // default
                       mobile:       false,       // default
                       live:         true        // default
-                    }
-                    )
-                    wow.init();
-    	$('.image-background').css("height", 300+$(".mastContent").height());
-    	$(window).scroll(function() {
-    		
-    		var hT = [];
-    		var hH = [];
+                  }
+                  )
+        wow.init();
+        $('.image-background').css("height", 300+$(".mastContent").height());
+        $(window).scroll(function() {
 
-    		for(var i=0;i<$('.employeeThreeList').length;i++){
-    			hT[i] = $($('.employeeThreeList')[i]).offset().top;
-		       	hH[i] = $($('.employeeThreeList')[i]).outerHeight();
-		       	var wH = $(window).height();
-		       	var wS = $(this).scrollTop();
-			   	
-		  		if (wS > (hT[i]+hH[i]-wH)){
-		   			
-		   		}
-		
-    		}
-    	});
+          var hT = [];
+          var hH = [];
+
+          for(var i=0;i<$('.employeeThreeList').length;i++){
+             hT[i] = $($('.employeeThreeList')[i]).offset().top;
+             hH[i] = $($('.employeeThreeList')[i]).outerHeight();
+             var wH = $(window).height();
+             var wS = $(this).scrollTop();
+
+             if (wS > (hT[i]+hH[i]-wH)){
+
+             }
+
+         }
+     });
     });
 })();
